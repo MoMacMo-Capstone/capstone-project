4c535fe01eaa111443fa941f7a560f0c329104f7
Nathan Koppel <nathankoppel0@gmail.com>
2025-04-04 09:12:35 -0500

    Split off 'model.py' file

---
+++ test_fid.py
@@ -0,0 +1,46 @@
+import torch
+from torchmetrics.image.fid import FrechetInceptionDistance
+
+import read_seismic_data
+
+def color_images(images):
+    images = images / (images.abs().amax(2, keepdim=True).amax(3, keepdim=True) + 1e-9)
+    images = images * 2
+    return torch.cat([images, images.abs() - 1, -images], dim=1).clamp(0, 1)
+
+def prepare_for_fid(imgs):
+    return (color_images(imgs) * 128).clamp(0, 255).to(torch.uint8)
+
+resolution = (64, 64)
+batch_size = 256
+
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+
+# transform = transforms.Compose([
+    # transforms.ToTensor(),
+    # transforms.Normalize((0.5,), (0.5,))
+# ])
+# train_dataset = datasets.MNIST(root='data', train=True, download=True, transform=transform)
+# train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
+fid_metric_acc = FrechetInceptionDistance(feature = 2048).to(device)
+fid_metric = FrechetInceptionDistance(feature = 2048).to(device)
+
+epoch = 0
+
+while True:
+    epoch += 1
+
+    imgs_1 = read_seismic_data.get_chunks(batch_size, resolution[0])
+    imgs_1 = torch.tensor(imgs_1, device=device)
+
+    imgs_2 = read_seismic_data.get_chunks(batch_size, resolution[0])
+    imgs_2 = torch.tensor(imgs_2, device=device)
+
+    print(imgs_1.shape, prepare_for_fid(imgs_1).shape)
+    fid_metric_acc.update(prepare_for_fid(imgs_1), real = True)
+    fid_metric.reset()
+    fid_metric.merge_state(fid_metric_acc)
+    fid_metric.update(prepare_for_fid(imgs_2), real = False)
+    fid_score = fid_metric.compute()
+    print(f"FID: {fid_score}")
---
+++ train_mean_stdev.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+# from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import torchvision
+
+import lama_mask
+import read_seismic_data
+from model import *
+
+batch_size = 256
+step = 0
+steps = 5000
+
+Mean = MeanEstimator().to(device)
+Stdev = VarEstimator().to(device)
+
+print("Mean params:", sum(p.numel() for p in Mean.parameters()))
+print("Stdev params:", sum(p.numel() for p in Stdev.parameters()))
+
+writer = SummaryWriter()
+
+hparams = {
+    "lr": 1e-3,
+    "beta1": 0.9,
+    "beta2": 0.99,
+    "Steps": 5000,
+    "Batch size": batch_size,
+    "Mean params": sum(p.numel() for p in Mean.parameters()),
+    "Stdev params": sum(p.numel() for p in Stdev.parameters()),
+}
+checkpoint = None
+
+if checkpoint:
+    checkpoint = torch.load(checkpoint)
+    Mean.load_state_dict(checkpoint["mean"])
+    Stdev.load_state_dict(checkpoint["stdev"])
+    step = checkpoint["step"]
+
+for name, value in hparams.items():
+    writer.add_scalar(f"hparams/{name}", value, 0)
+
+optimizer_M_STD = optim.AdamW(list(Mean.parameters()) + list(Stdev.parameters()), lr=1e-3, betas=(0.9, 0.99))
+
+while step < hparams["Steps"]:
+    step += 1
+
+    real_imgs = read_seismic_data.get_chunks(batch_size, resolution[0])
+    real_imgs = abs_normalize(torch.tensor(real_imgs, device=device))
+    real_imgs = real_imgs.detach().requires_grad_(True)
+
+    mask = lama_mask.make_seismic_masks(batch_size, resolution)
+    mask = torch.tensor(mask, device=device)
+
+    mean = Mean(real_imgs, mask)
+    stdev = Stdev(mean.detach(), mask)
+
+    optimizer_M_STD.zero_grad()
+    M_Loss = (mean - real_imgs).square().mean()
+    STD_Loss = (stdev.square() - (mean - real_imgs).square()).square().mean()
+    (M_Loss + STD_Loss).backward()
+    optimizer_M_STD.step()
+
+    writer.add_scalar("Loss/Mean loss", M_Loss, step)
+    writer.add_scalar("Loss/Stdev loss", STD_Loss, step)
+
+    grid = torch.cat([
+        real_imgs[:32, 0:1],
+        stdev[:32, 0:1],
+        mean[:32, 0:1],
+    ], 0)
+    grid = nn.functional.interpolate(grid, scale_factor=2, mode="nearest")
+    grid = color_images(grid)
+    grid = torchvision.utils.make_grid(grid, nrow=32)
+    writer.add_image("Images", grid, step)
+
+    print(f"Step {step}: Mean Loss: {M_Loss.item():.05}, Stdev Loss: {STD_Loss.item():.05}")
+
+    if step % 200 == 0:
+        torch.save({
+            "mean": Mean.state_dict(),
+            "stdev": Stdev.state_dict(),
+            "step": step,
+        }, f"m_std_checkpoint_{step}.ckpt")
